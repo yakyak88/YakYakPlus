c53545db569cd7ee68183c1a7a031294e5a949d0
yakir <danielyakir01@gmail.com>
2022-10-12 14:37:15 +0300

    update code

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import Search from "./components/Search";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Watch from "./components/Watch";
+import Tv from "./components/Tv";
+import Episodes from "./components/Episodes";
+import Series from "./components/Series";
+import Movies from "./components/Movies";
+import EpisodeDetails from "./components/EpisodeDetails";
+
+function App() {
+    return (
+        <div className="App ">
+            <Router>
+                <Header></Header>
+                <Routes>
+                    <Route path="/home" element={<Home />}></Route>
+                    <Route path="/" element={<Home />}></Route>
+                    <Route path="/search" element={<Search />}></Route>
+                    <Route path="/watch" element={<Watch />}></Route>
+                    <Route path="/tv" element={<Tv />}></Route>
+                    <Route path="/movies" element={<Movies />}></Route>
+                    <Route path="/Series" element={<Series />}></Route>
+                    <Route
+                        path="/episosedetails"
+                        element={<EpisodeDetails />}
+                    ></Route>
+                    <Route
+                        path="/episodes"
+                        element={<Episodes></Episodes>}
+                    ></Route>
+                </Routes>
+            </Router>
+        </div>
+    );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap.bundle";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import "bootstrap/js/src/collapse.js";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
